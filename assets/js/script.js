244774a4c9606a83b69c4a78dccbc22cecb34966
Tell Aulin <tell@tellemontage.se>
2024-04-22 21:54:10 +0000

    Add function and styling for cards that are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 // Wait for the DOM to finish loading before running the game 
 document.addEventListener('DOMContentLoaded', function () {
 
+    // Add function for cards that are clicked
 
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            card.classList.add("clicked");
+
+        })
+    })
 })
