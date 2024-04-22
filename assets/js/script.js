fe110fc9887eb1c68feb5985a8f23769784691af
Tell Aulin <tell@tellemontage.se>
2024-04-22 20:02:13 +0000

    Change section gameboard to contain all playcard images and style of display for gameboard and cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
 // Wait for the DOM to finish loading before running the game 
 document.addEventListener('DOMContentLoaded', function () {
 
+            /*
+                // Create an array with cards
 
-    // Create an array with cards
-
-    let myCards = [{
-            name: "ace-black",
-            image: "assets/images/A-C.png"
-        },
-        {
-            name: "ace-red",
-            image: "assets/images/A-D.png"
-        },
-        {
-            name: "ace-black",
-            image: "assets/images/A-S.png"
-        }, {
-            name: "ace-red",
-            image: "assets/images/A-H.png"
-        },
-    ];
-
-
-    // Pushing in the array with cards
-
-    /*myCards.push(newDeckObject);*/
+                let myCards = [{
+                        name: "ace-black",
+                        image: "assets/images/A-C.png"
+                    },
+                    {
+                        name: "ace-red",
+                        image: "assets/images/A-D.png"
+                    },
+                    {
+                        name: "ace-black",
+                        image: "assets/images/A-S.png"
+                    }, {
+                        name: "ace-red",
+                        image: "assets/images/A-H.png"
+                    },
+                ];
 
 
+                // Pushing in the array with cards
 
-    // set up grid with playing cards on table
+                /*myCards.push(newDeckObject);
 
-    const container = document.querySelector(".cards-container");
 
-    function makeGrid() {
-        for (i = 0; i < 4; i++) {
-            const row = document.createElement("div");
-            row.className = "card";
-            container.appendChild(row);
 
-            for (j = 1; j < 4; j++) {
-                const col = document.createElement("div");
-                col.className = "card";
-                row.appendChild(col);
-            }
-        }
-    }
+                // set up grid with playing cards on table
 
-    makeGrid();
-})
+                const container = document.querySelector(".cards-container");
+
+                function makeGrid() {
+                    for (i = 0; i < 4; i++) {
+                        const row = document.createElement("div");
+                        row.className = "card";
+                        container.appendChild(row);
+
+                        for (j = 1; j < 4; j++) {
+                            const col = document.createElement("div");
+                            col.className = "card";
+                            row.appendChild(col);
+                        }
+                    }
+                }
+
+                makeGrid();
+            })
