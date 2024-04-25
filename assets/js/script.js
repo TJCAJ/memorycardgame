bf3532c6eafaa222fe44aa2ea5d85161f7f77273
Tell Aulin <tell@tellemontage.se>
2024-04-25 14:03:38 +0000

    Change modal play btn to close-modal-btn while add const closeModalButton in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const modal = document.querySelector("#modal")
     const openModalButton = document.querySelector("#open-modal-btn")
+    const closeModalButton = document.querySelector("#close-modal-btn")
     const overlay = document.querySelector("#overlay")
 
     openModalButton.addEventListener("click", () => {
         modal.classList.add("open")
         overlay.classList.add("open")
     })
-
+    closeModalButton.addEventListener("click", closeModal)
     overlay.addEventListener("click", closeModal)
 
     function closeModal() {
